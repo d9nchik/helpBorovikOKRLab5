2af8e4fa8c66ba4c14c41e1fa73c91a0a1601cd6
d9nich <dan.galaiko@gmail.com>
2020-12-11 21:30:21 +0200

    fixed bug in index.js

--- src/index.js
+++ src/index.js
@@ -3,23 +3,26 @@ import './js/pizzaChooserAreas.js';
 import './js/cart.js';
 import './js/service.js';
 import './js/order.js';
-import M from 'minimatch';
 import './css/main.css';
 
 //слайдер
 // по документації materialize слайдер
 document.addEventListener('DOMContentLoaded', function () {
     var sidenav = document.querySelectorAll('.sidenav');
+    // eslint-disable-next-line no-undef
     M.Sidenav.init(sidenav, { edge: 'left', draggable: true });
     var slider = document.querySelectorAll('.slider');
+    // eslint-disable-next-line no-undef
     M.Slider.init(slider, {
         height: 400,
         indicators: false,
         interval: 4000,
     });
     var MBox = document.querySelectorAll('.materialboxed');
+    // eslint-disable-next-line no-undef
     M.Materialbox.init(MBox);
     var parallax = document.querySelectorAll('.parallax');
+    // eslint-disable-next-line no-undef
     M.Parallax.init(parallax);
 });
 
